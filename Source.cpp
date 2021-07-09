95203a8406351951b16eabf8bd93fc0086aea8e6
Oluwakunle Eniola <eniolakunle10@gmail.com>
2021-07-09 11:56:09 -0400

    First commit in a while

--- Source.cpp
+++ Source.cpp
@@ -1,18 +1,22 @@
-#include "City.h"	
+#include "City.h"
 #include "Flight.h"
 #include "Airport.h"
 
+// Kunle new addition (git reintroduction)
 
-int main() {
+int main()
+{
 	string filename;
 	cout << "Type in a file with flights labeled in such order per line: \"DepartureCity ArrivalCity Mileage Cost\" " << endl;
-	cout << "Filename: "; getline(cin, filename);
+	cout << "Filename: ";
+	getline(cin, filename);
 	ifstream file(filename.c_str());
 	graphAirport Reagan;
 	Reagan.createGraph(file);
 	string city;
 	cout << "Type in the city from which you would like to peform a depth first traversal from. " << endl;
-	cout << "City: "; getline(cin, city);
+	cout << "City: ";
+	getline(cin, city);
 	Reagan.depthFirstTraversal(city);
 	cout << endl;
 	return 0;
